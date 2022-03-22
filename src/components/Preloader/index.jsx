b366307e3e887fb48bace669c58249e8e1d4876c
dripDAO <dripbonddao@gmail.com>
2022-03-22 17:52:23 -0500

    update

--- src/components/Preloader/index.jsx
+++ src/components/Preloader/index.jsx
@@ -21,7 +21,6 @@ function PreloaderContainer({Title}){
 	                name="viewport"
 	                content="width=device-width, initial-scale=1, shrink-to-fit=no"
 	            />
-	            <title>{Title}</title>
 	        </Helmet>
 		    <div id="preloader">
 		        <div className="preload-content">
