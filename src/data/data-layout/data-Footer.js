979ad9f53474ff4444ef14ea85dd8ac5a9e30a42
dripDAO <dripbonddao@gmail.com>
2022-03-21 20:13:13 -0500

    first

---
+++ src/data/data-layout/data-Footer.js
@@ -0,0 +1,102 @@
+import{
+
+	// FooterWithTestImg1,
+	// FooterWithTestImg2,
+	// FooterWithTestImg3,
+
+ //    FAQMember1,
+ //    FAQMember2,
+ //    FAQMember3,
+ //    FAQMember4,
+
+	// FooterPagesPartners1,
+	// FooterPagesPartners2,
+	// FooterPagesPartners3,
+	// FooterPagesPartners4,
+	// FooterPagesPartners5,
+} from '../../utils/allImgs'
+
+export const IcoName = [
+	{IcoName:'fa fa-facebook'},
+	{IcoName:'fa fa-twitter'},
+	{IcoName:'fa fa-google-plus'},
+	{IcoName:'fa fa-instagram'},
+	{IcoName:'fa fa-linkedin'}
+]
+
+export const TextFooter = [
+	{
+		title:'NAVIGATE',
+		text1:'Advertisers',
+		text2:'Developers',
+		text3:'Resources',
+		text4:'Company',
+		text5:'Connect',
+        classBlock:"col-12 col-lg-2 col-md-6",
+        classInfo:"contact_info mt-s text-center fadeInUp",
+	}
+]
+
+// export const TestimonialContent = [
+//     {
+//         img:FooterWithTestImg1,
+//         name:'Sunny Khan',
+//         title:'Head of Design, Company CEO'
+//     },
+//     {
+//         img:FooterWithTestImg2,
+//         name:'Ajoy Das',
+//         title:'Head of Idea, Company CEO'
+//     },
+//     {
+//         img:FooterWithTestImg3,
+//         name:'Jebin Khan',
+//         title:'Head of Marketing, Company CEO'
+//     },
+// ]
+
+// export const FQAInfo = [
+//     {
+//         text:'What are the objectives of this token?',
+//         ID:'Q1'
+//     },
+//     {
+//         text:'What is Token Sale and pre-sale?',
+//         ID:'Q2'
+//     },
+//     {
+//         text:'What is the pre-sale start date?',
+//         ID:'Q3'
+//     },
+//     {
+//         text:'how may I take part in pre-sale?',
+//         ID:'Q4'
+//     },
+//     {
+//         text:'What is the best features and services we deiver?',
+//         ID:'Q5'
+//     },
+// ]
+
+// export const TeamMemberContent = [
+//     {
+//         img:FAQMember1,
+//         name:'Randy crishen',
+//         title:'Company CEO'
+//     },
+//     {
+//         img:FAQMember2,
+//         name:'Monica Ashker',
+//         title:'Web Designer'
+//     },
+//     {
+//         img:FAQMember3,
+//         name:'Tollay Ramzomi',
+//         title:'Web Developer'
+//     },
+//     {
+//         img:FAQMember4,
+//         name:'Jacke Wilson',
+//         title:'Marketing Specialist'
+//     },
+// ]
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Page from '../containers/Page'
+
+const Home = () => {
+
+  return (
+    <>
+      <Page />
+    </>
+  );
+}
+
+export default Home;
+
